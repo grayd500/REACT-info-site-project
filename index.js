936a6527ba7f3900d3a0bdf86ef9a894c145b682
David <grayd500@gmail.com>
2023-12-30 14:31:49 -0500

    Update with initial js files

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+Challenge: Project setup
+
+- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+  Put the links to those fonts ABOVE the style.css link in index.html (Use
+  the `<link/>` elements instead of the @import or npm options for getting
+  the fonts. You may need to do some extra research to figure out how this 
+  works if you haven't done it before)
+*/
+
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
+
+
+
+
+
+
