6cb49b21b5fe27bafbfb4c02b88f10322d1b1dca
David <grayd500@gmail.com>
2023-12-30 15:10:28 -0500

    Update navbar styling

--- components/Main.js
+++ components/Main.js
@@ -1,5 +1,16 @@
 import React from "react"
 
 export default function Main() {
-return <h1>Main goes here</h1>
+return (
+<div className="container">
+    <h1>Fun facts about React</h1>
+    <ul className="nav-items">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+</div>
+)
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,11 @@
 import React from "react"
 
-export default function Main() {
-    return <h1>Main component</h1>
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../images/react-icon-small.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
 }
